629f0c1765a08032df1d84c5886897f792ba1f6f
everton.lacerda <everton.lacerda@sennit.com.br>
2021-02-16 18:49:43 -0300

    feat(modules): add modules shared

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './_guard/auth.guard';
 import { MessagesComponent } from './messages/messages.component';
 import { ListComponent } from './list/list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -8,10 +9,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'members', component: MemberListComponent },
-  { path: 'member/:id', component: MemberDetailComponent },
-  { path: 'lists', component: ListComponent },
-  { path: 'messages', component: MessagesComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'member/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListComponent },
+      { path: 'messages', component: MessagesComponent },
+    ],
+  },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { AccountService } from './../_services/account.service';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,17 +11,25 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
   register() {
-    this.accountService.register(this.model).subscribe(res => {
-      console.log(res)
-      this.cancel()
-    }, error => {
-      console.log(error)
-    })
+    this.accountService.register(this.model).subscribe(
+      (res) => {
+        console.log(res);
+        this.cancel();
+      },
+      (error) => {
+        this.toastr.error(error.error);
+
+        console.log(error);
+      }
+    );
   }
 
   cancel() {
